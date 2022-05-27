894ec2e8970680f3f37306a3dd8ba0ebef308066
Tyler Paulson <me@tylerpaulson.com>
2022-05-27 14:14:50 -0400

    Start plugin file

---
+++ donation-integrator.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: Donately Integrator
+Description: Integrates Donately campaigns into a page 
+Version:     0.0.1
+Author:      Tyler Paulson
+Author URI:  https://work.tylerpaulson.com
+License:     GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+defined( 'ABSPATH' ) or die('No script kiddies please!');
